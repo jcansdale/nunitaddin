579b2e20920a43f865925e57d33a52c46b824310
jcansdale <jcansdale@7ebd57d3-1131-0410-a91a-95ad2f9eeeed>
2008-12-26 16:59:02 +0000

    Tagging version 2.4.6

---
+++ tags/2.4/current/src/NUnitAddIn/tests/MockTestListener.cs
@@ -0,0 +1,49 @@
+﻿namespace NUnit.AddInRunner.Tests
+{
+    using TestDriven.Framework;
+    using System.Collections;
+
+    class MockTestListener : ITestListener
+    {
+        public int TestFinishedCount;
+        public int TestResultsUrlCount;
+        public int WriteLineCount;
+        public int SuccessCount;
+        public int FailureCount;
+        public int IgnoredCount;
+        ArrayList testResults = new ArrayList();
+
+        public TestResult[] TestResults
+        {
+            get { return (TestResult[])testResults.ToArray(typeof(TestResult)); }
+        }
+
+        public void TestFinished(TestResult summary)
+        {
+            this.testResults.Add(summary);
+            this.TestFinishedCount++;
+            switch (summary.State)
+            {
+                case TestState.Passed:
+                    this.SuccessCount++;
+                    break;
+                case TestState.Failed:
+                    this.FailureCount++;
+                    break;
+                case TestState.Ignored:
+                    this.IgnoredCount++;
+                    break;
+            }
+        }
+
+        public void TestResultsUrl(string resultsUrl)
+        {
+            this.TestResultsUrlCount++;
+        }
+
+        public void WriteLine(string text, Category category)
+        {
+            this.WriteLineCount++;
+        }
+    }
+}
