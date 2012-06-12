41d5b492e32abbf0000aa80e1d64e8de54aba6d0
Jamie Cansdale <jcansdale@users.noreply.github.com>
2012-06-12 09:49:09 +0000

    Updated assembly version (2.6.0).

--- src/NUnitAddIn/nunit-addin/Properties/AssemblyInfo.cs
+++ src/NUnitAddIn/nunit-addin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mutant Design Limited")]
 [assembly: AssemblyProduct("NUnit add-in for TestDriven.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.5.3.0")]
-[assembly: AssemblyFileVersion("2.5.3.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
