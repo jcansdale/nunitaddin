89490329b617a90591bd6cc681fe93e6815d24f0
Jamie Cansdale <jcansdale@users.noreply.github.com>
2015-07-09 14:56:47 +0000

    Build using .NET Framework v3.5.

--- vars.bat
+++ vars.bat
@@ -1,6 +1,8 @@
 @echo off
-@echo This build environment requires .NET Framework 2.0 RTM (v2.0.50727)
+@echo This build environment requires .NET Framework 3.5 RTM
 @echo To build use 'msbuild NUnitAddIn.proj'
 
-set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
-set path=%path%;%cd%\tools\WiX
+set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v3.5
+set path=%path%;%cd%\tools\WiX;%cd%\tools\NuGet
+
+set EnableNuGetPackageRestore=true
