7c738b482bfcc9c560a1008bdad4c7487588da29
Jamie Cansdale <jcansdale@users.noreply.github.com>
2016-06-07 12:11:35 +0000

    Compile with .NET Framework 3.5 version of MSBuild.

--- vars.bat
+++ vars.bat
@@ -1,6 +1,7 @@
 @echo off
-@echo This build environment requires .NET Framework 2.0 RTM (v2.0.50727)
-@echo To build use 'msbuild NUnitAddIn.proj' 
+@echo This build environment requires .NET Framework 3.5
+@echo To build use 'msbuild NUnitAddIn.proj'
 
-set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
+set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v3.5
 set path=%path%;%cd%\tools\WiX
+
